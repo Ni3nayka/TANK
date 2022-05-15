4b9fdd70d9e2487188977c5f02ea63bf97e3b552
Egor Bakay <egor_bakay@inbox.ru>
2022-05-15 10:27:41 +0300

    predzashita

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,14 +2,40 @@
 #include "motor.h"
 #include "voltmetr.h"
 
+#include <Servo.h>
+Servo gnu_tank;
+Servo povorot_bashni_gorizontal;
+Servo povorot_bashni_vertical;
+
+int servo_val = 90;
+int servo_gorizontal_val = 90;
+int servo_vertical_val = 90;
+
 void setup(){
   Serial.begin(9600);
   gamepad.setup();
+  gnu_tank.attach(9);
+  povorot_bashni_gorizontal.attach(10);
+  povorot_bashni_vertical.attach(A5);
 }
 
 void loop(){
   gamepad.update();
   motor(LEFT,  gamepad.joystick(PSS_LY));
   motor(RIGHT, gamepad.joystick(PSS_RY));
-  gamepad.vibration = gamepad.button(PSB_BLUE);
+
+  if (gamepad.button(PSB_BLUE)) servo_val++;
+  if (gamepad.button(PSB_GREEN)) servo_val--;
+  
+  if (gamepad.button(PSB_PAD_LEFT)) servo_gorizontal_val++;
+  if (gamepad.button(PSB_PAD_RIGHT)) servo_gorizontal_val--;
+  
+  if (gamepad.button(PSB_PAD_UP)) servo_vertical_val++;
+  if (gamepad.button(PSB_PAD_DOWN)) servo_vertical_val--;
+  
+  gnu_tank.write(servo_val);
+  povorot_bashni_gorizontal.write(servo_gorizontal_val);
+  povorot_bashni_vertical.write(servo_vertical_val);
+  
+  gamepad.vibration = gamepad.button(PSB_RED);
 }
--- src/main/motor.h
+++ src/main/motor.h
@@ -10,6 +10,24 @@ AF_DCMotor AF_DCMotor_motor_2(2);
 AF_DCMotor AF_DCMotor_motor_3(3);
 AF_DCMotor AF_DCMotor_motor_4(4);
 
+
+void motor(byte number, int speed) {
+  speed = map(constrain(speed,-100,100),-100,100,-255,255);
+  if (number==1) {
+    if (speed==0)     AF_DCMotor_motor_1.run(RELEASE);
+    else if (speed>0) AF_DCMotor_motor_1.run(FORWARD);
+    else              AF_DCMotor_motor_1.run(BACKWARD);
+    AF_DCMotor_motor_1.setSpeed(abs(speed));
+  }
+  else if (number==2) {
+    if (speed==0)     AF_DCMotor_motor_2.run(RELEASE); 
+    else if (speed>0) AF_DCMotor_motor_2.run(FORWARD); 
+    else              AF_DCMotor_motor_2.run(BACKWARD);
+    AF_DCMotor_motor_2.setSpeed(abs(speed));
+  }
+}
+
+/*
 void motor(byte number, int speed) {
   speed = map(constrain(speed,-100,100),-100,100,-255,255);
   if (number==1) {
@@ -26,4 +44,4 @@ void motor(byte number, int speed) {
     AF_DCMotor_motor_3.setSpeed(abs(speed));
     AF_DCMotor_motor_4.setSpeed(abs(speed));
   }
-}
+}*/
